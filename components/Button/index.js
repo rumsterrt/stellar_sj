c24c8baf281028154297ce272d571c51e81e664c
Anatoly Ryumkin <aryumkin@deloitte.com>
2020-07-17 15:34:54 +0900

    Design fixes

--- components/Button/index.js
+++ components/Button/index.js
@@ -2,12 +2,6 @@ import React from 'react'
 import { TouchableOpacity } from 'react-native'
 import './index.styl'
 
-const types = {
-  primary: 'primary',
-  secondary: 'secondary',
-  transparent: 'transparent'
-}
-
 const Button = ({ text, onPress = () => {}, style, type = '' }) => {
   return pug`
     TouchableOpacity.root(onPress=onPress style=style styleName=type)
