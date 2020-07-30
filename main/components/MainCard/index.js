047aedfaad17c75bdfdf565999fe876a652d2020
Anatoly Ryumkin <aryumkin@deloitte.com>
2020-07-30 17:02:11 +0900

    Add landing navbar

--- main/components/MainCard/index.js
+++ main/components/MainCard/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { View } from 'react-native'
 import './index.styl'
 
-const MainCard = ({ children, isFirst, isLast }) => {
+const MainCard = ({ children, isFirst, isLast, style, onLayout }) => {
   return pug`
-    View.root(styleName=[isFirst ? 'first' : '',isLast ? 'last' : ''])
+    View.root(styleName=[isFirst ? 'first' : '',isLast ? 'last' : ''] style=style onLayout=onLayout)
       = children
     `
 }
--- main/pages/PLanding/index.js
+++ main/pages/PLanding/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header'
 import Intro from '../../components/Landing/Intro'
 import FirstSection from '../../components/Landing/FirstSection'
@@ -6,15 +6,23 @@ import SecondSection from '../../components/Landing/SecondSection'
 import GetStarted from '../../components/Landing/GetStarted'
 import Footer from '../../components/Footer'
 import MainCard from '../../components/MainCard'
-import { Image, Text } from 'react-native'
+import { Image, Text, Dimensions } from 'react-native'
 import { emit } from 'startupjs'
-import { Link } from 'components'
+import { Link, Button } from 'components'
 import { BASE_URL } from '@env'
 import './index.styl'
 
 const base = BASE_URL
 
+const navItems = ['Introduction', 'First Section', 'Second Section', 'Get Started']
+
 const PLanding = () => {
+  const [selectedNav, setSelectedNav] = useState(0)
+
+  const handleNavItemPress = (index) => () => {
+    setSelectedNav(index)
+  }
+
   return pug`
     Header  
       Image.rocket(source={uri:base + '/img/logo.svg'})
@@ -28,7 +36,15 @@ const PLanding = () => {
         = ' for '
         Link(onPress=() => emit('url', 'https://twitter.com/ajlkn'))
           = 'HTML5 UP'
-    MainCard(isFirst=true)
+    MainCard.nav(isFirst=true)
+      each navItem, index in navItems
+        Button.navItem(
+          key=navItem
+          text=navItem
+          styleName=[{first: index === 0, active: index === selectedNav}]
+          onPress=handleNavItemPress(index)
+        )
+    MainCard(isFirst=Dimensions.get('window').width < 700)
       Intro
     MainCard
       FirstSection
