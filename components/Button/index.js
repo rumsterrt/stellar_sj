cfae027216693576cb58ae24e5bb8315f73a0724
Anatoly Ryumkin <aryumkin@deloitte.com>
2020-07-30 17:02:00 +0900

    Fix Button text bug

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, Text } from 'react-native'
 import './index.styl'
 
 const Button = ({ text, onPress = () => {}, style, type = '' }) => {
   return pug`
     TouchableOpacity.root(onPress=onPress style=style styleName=type)
-      = text
+      Text.text(styleName=type) #{text}
     `
 }
 
